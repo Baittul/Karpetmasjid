de6fe9158c011a1b62dd94c97474cbd1734890c3
Ziopmsi <almuslimkarpet@gmail.com>
2024-08-30 15:21:11 +0700

    Add files via upload

---
+++ script-nav.js
@@ -0,0 +1,16 @@
+let lastScrollTop = 0;
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', function() {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    
+    if (scrollTop > lastScrollTop) {
+        // Scroll down - hide navbar
+        navbar.style.top = "-80px";
+    } else {
+        // Scroll up - show navbar
+        navbar.style.top = "0";
+    }
+
+    lastScrollTop = scrollTop;
+});
